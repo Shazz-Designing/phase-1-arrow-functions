45d6063ad72ef5fe5130ff9d3b3f585e4a315b5c
sharon <sapot@safaricom.co.ke>
2023-10-18 08:12:47 +0300

    Use the functions

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const divide = (x, y) => x / y;
 const square = x => x ** 2;
 
 const add = (x, y) => x + y;
+
+console.log(divide(10, 2)); // To print 5
+console.log(square(4)); // To print 16
+console.log(add(3, 7)); // To print 10
